180826a15eafb1a01c91832ca6d80c24a557e11e
veip007 <12257917+veip007@users.noreply.github.com>
2021-10-25 17:52:35 +0800

    签到领现金

--- jd/chinnkarahoi_jd_scripts_jd_cash.js
+++ jd/chinnkarahoi_jd_scripts_jd_cash.js
@@ -47,7 +47,7 @@ let allMessage = '';
 !(async () => {
   if (!cookiesArr[0]) {
     $.msg($.name, '【提示】请先获取京东账号一cookie\n直接使用NobyDa的京东签到获取', 'https://bean.m.jd.com/bean/signIndex.action', {"open-url": "https://bean.m.jd.com/bean/signIndex.action"});
-    return;
+    return; 
   }
   await requireConfig()
   await getAuthorShareCode();
