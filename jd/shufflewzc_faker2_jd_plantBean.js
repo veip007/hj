455d53a5228ace23084b4709ecf5940449d02ce0
veip007 <12257917+veip007@users.noreply.github.com>
2021-10-25 17:56:42 +0800

    京东种豆得豆

--- jd/shufflewzc_faker2_jd_plantBean.js
+++ jd/shufflewzc_faker2_jd_plantBean.js
@@ -14,7 +14,7 @@
 =====================================Loon================================
 [Script]
 cron "1 7-21/2 * * *" script-path=https://raw.githubusercontent.com/Aaron-lv/sync/jd_scripts/jd_plantBean.js,tag=京东种豆得豆
-
+ 
 ======================================Surge==========================
 京东种豆得豆 = type=cron,cronexp="1 7-21/2 * * *",wake-system=1,timeout=3600,script-path=https://raw.githubusercontent.com/Aaron-lv/sync/jd_scripts/jd_plantBean.js
 
