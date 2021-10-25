76b8d7a738c34a3d0d0cc748856ca8fd3167faff
veip007 <12257917+veip007@users.noreply.github.com>
2021-10-25 17:57:40 +0800

    东东农场

--- jd/yuannian1112_jd_scripts_jd_fruit.js
+++ jd/yuannian1112_jd_scripts_jd_fruit.js
@@ -9,7 +9,7 @@
 一天只能帮助3个人。多出的助力码无效
 ==========================Quantumultx=========================
 [task_local]
-#jd免费水果
+#jd免费水果 
 5 6-18/6 * * * jd_fruit.js, tag=东东农场, img-url=https://raw.githubusercontent.com/58xinian/icon/master/jdnc.png, enabled=true
 =========================Loon=============================
 [Script]
