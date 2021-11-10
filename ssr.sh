5f97c8c96a91143aeb7b2751bc0f57672e63985b
veip007 <12257917+veip007@users.noreply.github.com>
2021-11-10 22:57:34 +0800

    Add files via upload

---
+++ ssr.sh
@@ -0,0 +1,2 @@
+#!/usr/bin/env bash
+wget -O /www/wwwroot/www.lefu.men/ssr.txt https://raw.githubusercontent.com/learnhard-cn/free_proxy_ss/main/free && wget -O /www/wwwroot/www.lefu.men/ssr-1.txt  https://raw.githubusercontent.com/learnhard-cn/free_proxy_ss/main/v2ray/v2raysub && wget -O /www/wwwroot/www.lefu.men/ssr-2.txt  https://raw.githubusercontent.com/gushushen/free/main/ss && wget -O /www/wwwroot/www.lefu.men/ssr-3.txt  https://raw.githubusercontent.com/aiboboxx/v2rayfree/main/v2
