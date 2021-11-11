06d9b182c821c64523756f7772928af1791cc20b
veip007 <12257917+veip007@users.noreply.github.com>
2021-11-11 11:32:53 +0800

    Update ssr.sh

--- ssr.sh
+++ ssr.sh
@@ -1,2 +1,3 @@
-#!/usr/bin/env bash
+#!/bin/bash
+# 自用SSR订阅脚本
 wget -O /www/wwwroot/www.lefu.men/ssr.txt https://raw.githubusercontent.com/learnhard-cn/free_proxy_ss/main/free && wget -O /www/wwwroot/www.lefu.men/ssr-1.txt https://raw.githubusercontent.com/learnhard-cn/free_proxy_ss/main/v2ray/v2raysub && wget -O /www/wwwroot/www.lefu.men/ssr-2.txt https://raw.githubusercontent.com/gushushen/free/main/ss && wget -O /www/wwwroot/www.lefu.men/ssr-3.txt https://raw.githubusercontent.com/aiboboxx/v2rayfree/main/v2
