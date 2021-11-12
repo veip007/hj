659614c8696f4fcdb6e01bc529ad8be058caae86
veip007 <12257917+veip007@users.noreply.github.com>
2021-11-12 10:12:08 +0800

    Update qlzlm.sh

--- qlzlm.sh
+++ qlzlm.sh
@@ -1,31 +1,28 @@
 #!/bin/bash
 # 东东工厂
-cd /ql/scripts && rm shufflewzc_faker2_jd_jdfactory.js yuannian1112_jd_scripts_jd_jdfactory.js 
-cd /ql/scripts && wget https://raw.githubusercontent.com/veip007/hj/main/jd/yuannian1112_jd_scripts_jd_jdfactory.js
+rm /ql/scripts/shufflewzc_faker2/jd_jdfactory.js && wget -O /ql/scripts/yuannian1112_jd_scripts/jd_jdfactory.js https://raw.githubusercontent.com/veip007/hj/main/jd/yuannian1112_jd_scripts/jd_jdfactory.js
 # 口袋书店
-cd /ql/scripts && rm shufflewzc_faker2_jd_bookshop.js 
-cd /ql/scripts && wget https://raw.githubusercontent.com/veip007/hj/main/jd/shufflewzc_faker2_jd_bookshop.js
+wget -O /ql/scripts/shufflewzc_faker2/jd_bookshop.js https://raw.githubusercontent.com/veip007/hj/main/jd/shufflewzc_faker2/jd_bookshop.js
 # 签到领现金
-cd /ql/scripts && rm yuannian1112_jd_scripts_jd_cash.js shufflewzc_faker2_jd_cash.js 
-cd /ql/scripts && wget https://raw.githubusercontent.com/veip007/hj/main/jd/yuannian1112_jd_scripts_jd_cash.js
+rm /ql/scripts/shufflewzc_faker2/jd_cash.js 
+&& wget -O /ql/scripts/yuannian1112_jd_scripts/jd_cash.js https://raw.githubusercontent.com/veip007/hj/main/jd/yuannian1112_jd_scripts/jd_cash.js
 # 京喜工厂
-cd /ql/scripts && rm yuannian1112_jd_scripts_jd_dreamFactory.js shufflewzc_faker2_jd_dreamFactory.js 
-cd /ql/scripts && wget https://raw.githubusercontent.com/veip007/hj/main/jd/yuannian1112_jd_scripts_jd_dreamFactory.js 
+rm /ql/scripts/shufflewzc_faker2/jd_dreamFactory.js 
+&& wget -O /ql/scripts/yuannian1112_jd_scripts/jd_dreamFactory.js https://raw.githubusercontent.com/veip007/hj/main/jd/yuannian1112_jd_scripts/jd_dreamFactory.js 
 # 东东农场
-cd /ql/scripts && rm yuannian1112_jd_scripts_jd_fruit.js shufflewzc_faker2_jd_fruit.js 
-cd /ql/scripts && wget https://raw.githubusercontent.com/veip007/hj/main/jd/shufflewzc_faker2_jd_fruit.js
+rm /ql/scripts/shufflewzc_faker2/jd_fruit.js 
+&& wget -O /ql/scripts/yuannian1112_jd_scripts/jd_fruit.js https://raw.githubusercontent.com/veip007/hj/main/jd/shufflewzc_faker2/jd_fruit.js
 # 东东健康社区
-cd /ql/scripts && rm yuannian1112_jd_scripts_jd_health.js shufflewzc_faker2_jd_health.js 
-cd /ql/scripts && wget https://raw.githubusercontent.com/veip007/hj/main/jd/yuannian1112_jd_scripts_jd_health.js 
+rm /ql/scripts/shufflewzc_faker2/jd_health.js 
+&& wget -O /ql/scripts/yuannian1112_jd_scripts/jd_health.js https://raw.githubusercontent.com/veip007/hj/main/jd/yuannian1112_jd_scripts/jd_health.js 
 # 东东萌宠
-cd /ql/scripts && rm yuannian1112_jd_scripts_jd_pet.js shufflewzc_faker2_jd_pet.js 
-cd /ql/scripts && wget https://raw.githubusercontent.com/veip007/hj/main/jd/yuannian1112_jd_scripts_jd_pet.js 
+rm /ql/scripts/shufflewzc_faker2/jd_pet.js 
+&& wget -O /ql/scripts/yuannian1112_jd_scripts/jd_pet.js https://raw.githubusercontent.com/veip007/hj/main/jd/yuannian1112_jd_scripts/jd_pet.js 
 # 京东种豆得豆
-cd /ql/scripts && rm yuannian1112_jd_scripts_jd_plantBean.js shufflewzc_faker2_jd_plantBean.js 
-cd /ql/scripts && wget https://raw.githubusercontent.com/veip007/hj/main/jd/yuannian1112_jd_scripts_jd_plantBean.js 
+rm /ql/scripts/shufflewzc_faker2/jd_plantBean.js 
+&& wget -O /ql/scripts/yuannian1112_jd_scripts/jd_plantBean.js https://raw.githubusercontent.com/veip007/hj/main/jd/yuannian1112_jd_scripts/jd_plantBean.js 
 # 闪购盲盒
-cd /ql/scripts && rm yuannian1112_jd_scripts_jd_sgmh.js shufflewzc_faker2_jd_sgmh.js 
-cd /ql/scripts && wget https://raw.githubusercontent.com/veip007/hj/main/jd/yuannian1112_jd_scripts_jd_sgmh.js 
+rm /ql/scripts/shufflewzc_faker2/jd_sgmh.js 
+&& wget -O /ql/scripts/yuannian1112_jd_scripts/jd_sgmh.js https://raw.githubusercontent.com/veip007/hj/main/jd/yuannian1112_jd_scripts/jd_sgmh.js 
 # 京喜财富岛
-cd /ql/scripts && rm he1pu_JDHelp_jd_cfd.js 
-cd /ql/scripts && wget https://raw.githubusercontent.com/veip007/hj/main/jd/he1pu_JDHelp_jd_cfd.js
+wget -O /ql/scripts/he1pu_JDHelp/jd_cfd.js https://raw.githubusercontent.com/veip007/hj/main/jd/he1pu_JDHelp/jd_cfd.js
