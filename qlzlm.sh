88d830fd2988d986e7e1759d0bcfe116a61b3bee
veip007 <12257917+veip007@users.noreply.github.com>
2021-11-12 10:20:03 +0800

    Update qlzlm.sh

--- qlzlm.sh
+++ qlzlm.sh
@@ -4,25 +4,18 @@ rm /ql/scripts/shufflewzc_faker2/jd_jdfactory.js && wget -O /ql/scripts/yuannian
 # 口袋书店
 wget -O /ql/scripts/shufflewzc_faker2/jd_bookshop.js https://raw.githubusercontent.com/veip007/hj/main/jd/shufflewzc_faker2/jd_bookshop.js
 # 签到领现金
-rm /ql/scripts/shufflewzc_faker2/jd_cash.js 
-&& wget -O /ql/scripts/yuannian1112_jd_scripts/jd_cash.js https://raw.githubusercontent.com/veip007/hj/main/jd/yuannian1112_jd_scripts/jd_cash.js
+rm /ql/scripts/shufflewzc_faker2/jd_cash.js && wget -O /ql/scripts/yuannian1112_jd_scripts/jd_cash.js https://raw.githubusercontent.com/veip007/hj/main/jd/yuannian1112_jd_scripts/jd_cash.js
 # 京喜工厂
-rm /ql/scripts/shufflewzc_faker2/jd_dreamFactory.js 
-&& wget -O /ql/scripts/yuannian1112_jd_scripts/jd_dreamFactory.js https://raw.githubusercontent.com/veip007/hj/main/jd/yuannian1112_jd_scripts/jd_dreamFactory.js 
+rm /ql/scripts/shufflewzc_faker2/jd_dreamFactory.js && wget -O /ql/scripts/yuannian1112_jd_scripts/jd_dreamFactory.js https://raw.githubusercontent.com/veip007/hj/main/jd/yuannian1112_jd_scripts/jd_dreamFactory.js
 # 东东农场
-rm /ql/scripts/shufflewzc_faker2/jd_fruit.js 
-&& wget -O /ql/scripts/yuannian1112_jd_scripts/jd_fruit.js https://raw.githubusercontent.com/veip007/hj/main/jd/shufflewzc_faker2/jd_fruit.js
+rm /ql/scripts/shufflewzc_faker2/jd_fruit.js && wget -O /ql/scripts/yuannian1112_jd_scripts/jd_fruit.js https://raw.githubusercontent.com/veip007/hj/main/jd/shufflewzc_faker2/jd_fruit.js
 # 东东健康社区
-rm /ql/scripts/shufflewzc_faker2/jd_health.js 
-&& wget -O /ql/scripts/yuannian1112_jd_scripts/jd_health.js https://raw.githubusercontent.com/veip007/hj/main/jd/yuannian1112_jd_scripts/jd_health.js 
+rm /ql/scripts/shufflewzc_faker2/jd_health.js && wget -O /ql/scripts/yuannian1112_jd_scripts/jd_health.js https://raw.githubusercontent.com/veip007/hj/main/jd/yuannian1112_jd_scripts/jd_health.js
 # 东东萌宠
-rm /ql/scripts/shufflewzc_faker2/jd_pet.js 
-&& wget -O /ql/scripts/yuannian1112_jd_scripts/jd_pet.js https://raw.githubusercontent.com/veip007/hj/main/jd/yuannian1112_jd_scripts/jd_pet.js 
+rm /ql/scripts/shufflewzc_faker2/jd_pet.js && wget -O /ql/scripts/yuannian1112_jd_scripts/jd_pet.js https://raw.githubusercontent.com/veip007/hj/main/jd/yuannian1112_jd_scripts/jd_pet.js
 # 京东种豆得豆
-rm /ql/scripts/shufflewzc_faker2/jd_plantBean.js 
-&& wget -O /ql/scripts/yuannian1112_jd_scripts/jd_plantBean.js https://raw.githubusercontent.com/veip007/hj/main/jd/yuannian1112_jd_scripts/jd_plantBean.js 
+rm /ql/scripts/shufflewzc_faker2/jd_plantBean.js && wget -O /ql/scripts/yuannian1112_jd_scripts/jd_plantBean.js https://raw.githubusercontent.com/veip007/hj/main/jd/yuannian1112_jd_scripts/jd_plantBean.js
 # 闪购盲盒
-rm /ql/scripts/shufflewzc_faker2/jd_sgmh.js 
-&& wget -O /ql/scripts/yuannian1112_jd_scripts/jd_sgmh.js https://raw.githubusercontent.com/veip007/hj/main/jd/yuannian1112_jd_scripts/jd_sgmh.js 
+rm /ql/scripts/shufflewzc_faker2/jd_sgmh.js && wget -O /ql/scripts/yuannian1112_jd_scripts/jd_sgmh.js https://raw.githubusercontent.com/veip007/hj/main/jd/yuannian1112_jd_scripts/jd_sgmh.js
 # 京喜财富岛
 wget -O /ql/scripts/he1pu_JDHelp/jd_cfd.js https://raw.githubusercontent.com/veip007/hj/main/jd/he1pu_JDHelp/jd_cfd.js
