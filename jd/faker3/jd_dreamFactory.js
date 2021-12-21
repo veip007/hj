f79a93845b9491545a5ab6e791fa6d3b78a3681c
veip007 <12257917+veip007@users.noreply.github.com>
2021-12-21 09:43:37 +0800

    京喜工厂

--- jd/faker3/jd_dreamFactory.js
+++ jd/faker3/jd_dreamFactory.js
@@ -67,7 +67,7 @@ if ($.isNode()) {
     $.msg($.name, '【提示】请先获取京东账号一cookie\n直接使用NobyDa的京东签到获取', 'https://bean.m.jd.com/bean/signIndex.action', {"open-url": "https://bean.m.jd.com/bean/signIndex.action"});
     return;
   }
-  await requestAlgo();
+  await requestAlgo(); 
   await getActiveId();//自动获取每期拼团活动ID
   for (let i = 0; i < cookiesArr.length; i++) {
     if (cookiesArr[i]) {
