30d6fba3e2a9885742136fb836641d567cb99b25
veip007 <12257917+veip007@users.noreply.github.com>
2021-12-27 21:23:27 +0800

    Add files via upload

---
+++ ssr.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+bash <(curl -s -L https://raw.githubusercontent.com/veip007/hj/main/ssrdy.sh)
+python3 <(curl -s -L https://raw.githubusercontent.com/veip007/hj/main/ssr.py)
+find /www/wwwroot/www.lefu.men/ -name 'ssr.txt' | xargs perl -pi -e 's|#github.com/v2rayfree|#lefu.men 乐福门|g'
