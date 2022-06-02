ba5355c7c098fe10550937f07adb7a14ef30d6db
veip007 <12257917+veip007@users.noreply.github.com>
2022-06-02 18:45:40 +0800

    Update qlzlm.sh

--- qlzlm.sh
+++ qlzlm.sh
@@ -1,30 +1,29 @@
 #!/bin/bash
 # 东东工厂
-#wget -O /root/ql/scripts/shufflewzc_faker3_main/jd_jdfactory.js https://github.com/veip007/hj/raw/main/jd/faker3/jd_jdfactory.js && wget -O /root/ql/scripts/yuannian1112_jd_scripts/jd_jdfactory.js https://github.com/veip007/hj/raw/main/jd/faker3/jd_jdfactory.js
+#wget -O /root/ql/scripts/shufflewzc_faker3_main/jd_jdfactory.js https://raw.githubusercontent.com/veip007/hj/main/jd/faker3/jd_jdfactory.js && wget -O /root/ql/scripts/yuannian1112_jd_scripts/jd_jdfactory.js https://raw.githubusercontent.com/veip007/hj/main/jd/faker3/jd_jdfactory.js
 # 口袋书店
-#wget -O /root/ql/scripts/shufflewzc_faker3_main/jd_bookshop.js https://github.com/veip007/hj/raw/main/jd/faker3/jd_bookshop.js
+#wget -O /root/ql/scripts/shufflewzc_faker3_main/jd_bookshop.js https://raw.githubusercontent.com/veip007/hj/main/jd/faker3/jd_bookshop.js
 # 签到领现金
-wget -O /root/ql/scripts/shufflewzc_faker3_main/jd_cash.js https://github.com/veip007/hj/raw/main/jd/faker3/jd_cash.js && wget -O /root/ql/scripts/yuannian1112_jd_scripts/jd_cash.js https://github.com/veip007/hj/raw/main/jd/faker3/jd_cash.js
+wget -O /root/ql/scripts/shufflewzc_faker3_main/jd_cash.js https://raw.githubusercontent.com/veip007/hj/main/jd/faker3/jd_cash.js && wget -O /root/ql/scripts/yuannian1112_jd_scripts/jd_cash.js https://raw.githubusercontent.com/veip007/hj/main/jd/faker3/jd_cash.js
 # 京喜工厂
-wget -O /root/ql/scripts/shufflewzc_faker3_main/jd_dreamFactory_Mod.js https://github.com/veip007/hj/raw/main/jd/faker3/jd_dreamFactory_Mod.js && wget -O /root/ql/scripts/ccwav_QLScript2/jd_dreamFactory_Mod.js https://github.com/veip007/hj/raw/main/jd/faker3/jd_dreamFactory_Mod.js
+wget -O /root/ql/scripts/shufflewzc_faker3_main/jd_dreamFactory_Mod.js https://raw.githubusercontent.com/veip007/hj/main/jd/faker3/jd_dreamFactory_Mod.js && wget -O /root/ql/scripts/ccwav_QLScript2/jd_dreamFactory_Mod.js https://raw.githubusercontent.com/veip007/hj/main/jd/faker3/jd_dreamFactory_Mod.js
 # 东东农场
-wget -O /root/ql/scripts/shufflewzc_faker3_main/jd_fruit.js https://github.com/veip007/hj/raw/main/jd/faker3/jd_fruit.js && wget -O /root/ql/scripts/okyyds_yydspure_master/jd_fruit.js https://github.com/veip007/hj/raw/main/jd/faker3/jd_fruit.js && wget -O /root/ql/scripts/yuannian1112_jd_scripts/jd_fruit.js https://github.com/veip007/hj/raw/main/jd/faker3/jd_fruit.js
+wget -O /root/ql/scripts/shufflewzc_faker3_main/jd_fruit.js https://raw.githubusercontent.com/veip007/hj/main/jd/faker3/jd_fruit.js && wget -O /root/ql/scripts/okyyds_yydspure_master/jd_fruit.js https://raw.githubusercontent.com/veip007/hj/main/jd/faker3/jd_fruit.js && wget -O /root/ql/scripts/yuannian1112_jd_scripts/jd_fruit.js https://raw.githubusercontent.com/veip007/hj/main/jd/faker3/jd_fruit.js
 # 东东健康社区
-wget -O /root/ql/scripts/shufflewzc_faker3_main/jd_health.js https://github.com/veip007/hj/raw/main/jd/faker3/jd_health_help.js && wget -O /root/ql/scripts/okyyds_yydspure_master/jd_health.js https://github.com/veip007/hj/raw/main/jd/faker3/jd_health_help.js 
+wget -O /root/ql/scripts/shufflewzc_faker3_main/jd_health.js https://raw.githubusercontent.com/veip007/hj/main/jd/faker3/jd_health.js && wget -O /root/ql/scripts/okyyds_yydspure_master/jd_health.js https://raw.githubusercontent.com/veip007/hj/main/jd/faker3/jd_health.js 
 # 东东萌宠
-wget -O /root/ql/scripts/shufflewzc_faker3_main/jd_pet.js https://github.com/veip007/hj/raw/main/jd/faker3/jd_pet.js && wget -O /root/ql/scripts/okyyds_yydspure_master/jd_pet.js https://github.com/veip007/hj/raw/main/jd/faker3/jd_pet.js && wget -O /root/ql/scripts/yuannian1112_jd_scripts/jd_pet.js https://github.com/veip007/hj/raw/main/jd/faker3/jd_pet.js
+wget -O /root/ql/scripts/shufflewzc_faker3_main/jd_pet.js https://raw.githubusercontent.com/veip007/hj/main/jd/faker3/jd_pet.js && wget -O /root/ql/scripts/okyyds_yydspure_master/jd_pet.js https://raw.githubusercontent.com/veip007/hj/main/jd/faker3/jd_pet.js && wget -O /root/ql/scripts/yuannian1112_jd_scripts/jd_pet.js https://raw.githubusercontent.com/veip007/hj/main/jd/faker3/jd_pet.js
 # 京东种豆得豆
-wget -O /root/ql/scripts/shufflewzc_faker3_main/jd_plantBean.js https://github.com/veip007/hj/raw/main/jd/faker3/jd_plantBean.js && wget -O /root/ql/scripts/okyyds_yydspure_master/jd_plantBean.js https://github.com/veip007/hj/raw/main/jd/faker3/jd_plantBean.js && wget -O /root/ql/scripts/yuannian1112_jd_scripts/jd_plantBean.js https://github.com/veip007/hj/raw/main/jd/faker3/jd_plantBean.js
+wget -O /root/ql/scripts/shufflewzc_faker3_main/jd_plantBean.js https://raw.githubusercontent.com/veip007/hj/main/jd/faker3/jd_plantBean.js && wget -O /root/ql/scripts/okyyds_yydspure_master/jd_plantBean.js https://raw.githubusercontent.com/veip007/hj/main/jd/faker3/jd_plantBean.js && wget -O /root/ql/scripts/yuannian1112_jd_scripts/jd_plantBean.js https://raw.githubusercontent.com/veip007/hj/main/jd/faker3/jd_plantBean.js
 # 极速版签到
-wget -O /root/ql/scripts/shufflewzc_faker3_main/jd_speed_sign.js https://github.com/veip007/hj/raw/main/jd/faker3/jd_speed_sign.js && wget -O /root/ql/scripts/okyyds_yydspure_master/jd_speed_sign.js https://github.com/veip007/hj/raw/main/jd/faker3/jd_speed_sign.js && wget -O /root/ql/scripts/yuannian1112_jd_scripts/jd_speed_sign.js https://github.com/veip007/hj/raw/main/jd/faker3/jd_speed_sign.js
+wget -O /root/ql/scripts/shufflewzc_faker3_main/jd_speed_sign.js https://raw.githubusercontent.com/veip007/hj/main/jd/faker3/jd_speed_sign.js && wget -O /root/ql/scripts/okyyds_yydspure_master/jd_speed_sign.js https://raw.githubusercontent.com/veip007/hj/main/jd/faker3/jd_speed_sign.js && wget -O /root/ql/scripts/yuannian1112_jd_scripts/jd_speed_sign.js https://raw.githubusercontent.com/veip007/hj/main/jd/faker3/jd_speed_sign.js
 # 闪购盲盒
-#wget -O /root/ql/scripts/shufflewzc_faker3_main/jd_sgmh.js https://github.com/veip007/hj/raw/main/jd/faker3/jd_sgmh.js && wget -O /root/ql/scripts/yuannian1112_jd_scripts/jd_sgmh.js https://github.com/veip007/hj/raw/main/jd/faker3/jd_sgmh.js
+#wget -O /root/ql/scripts/shufflewzc_faker3_main/jd_sgmh.js https://raw.githubusercontent.com/veip007/hj/main/jd/faker3/jd_sgmh.js && wget -O /root/ql/scripts/yuannian1112_jd_scripts/jd_sgmh.js https://raw.githubusercontent.com/veip007/hj/main/jd/faker3/jd_sgmh.js
 # 京喜财富岛
-wget -O /root/ql/scripts/shufflewzc_faker3_main/jd_cfd.js https://github.com/veip007/hj/raw/main/jd/faker3/jd_cfd.js && wget -O /root/ql/scripts/okyyds_yydspure_master/jd_cfd.js https://github.com/veip007/hj/raw/main/jd/faker3/jd_cfd.js && wget -O /root/ql/scripts/yuannian1112_jd_scripts/jd_cfd.js https://github.com/veip007/hj/raw/main/jd/faker3/jd_cfd.js
+wget -O /root/ql/scripts/shufflewzc_faker3_main/jd_cfd.js https://raw.githubusercontent.com/veip007/hj/main/jd/faker3/jd_cfd.js && wget -O /root/ql/scripts/okyyds_yydspure_master/jd_cfd.js https://raw.githubusercontent.com/veip007/hj/main/jd/faker3/jd_cfd.js && wget -O /root/ql/scripts/yuannian1112_jd_scripts/jd_cfd.js https://raw.githubusercontent.com/veip007/hj/main/jd/faker3/jd_cfd.js
 #果园道家
-#wget -O /root/ql/scripts/shufflewzc_faker3_main/jddj_fruit.js https://github.com/veip007/hj/raw/main/jd/faker3/jddj_fruit.js && wget -O /root/ql/scripts/yuannian1112_jd_scripts/jddj_fruit.js https://github.com/veip007/hj/raw/main/jd/faker3/jddj_fruit.js
+#wget -O /root/ql/scripts/shufflewzc_faker3_main/jddj_fruit.js https://raw.githubusercontent.com/veip007/hj/main/jd/faker3/jddj_fruit.js && wget -O /root/ql/scripts/yuannian1112_jd_scripts/jddj_fruit.js https://raw.githubusercontent.com/veip007/hj/main/jd/faker3/jddj_fruit.js
 #极速版
-#wget -O /root/ql/scripts/yuannian1112_jd_scripts/jd_speed_redpocke.js https://github.com/veip007/hj/raw/main/jd/faker3/jd_speed_redpocke.js
-#wget -O /root/ql/scripts/yuannian1112_jd_scripts/jd_speed_signfaker.js https://github.com/veip007/hj/raw/main/jd/faker3/jd_speed_signfaker.js
-#wget -O /root/ql/scripts/yuannian1112_jd_scripts/jd_speed_signfree.js https://github.com/veip007/hj/raw/main/jd/faker3/jd_speed_signfree.js
-
+#wget -O /root/ql/scripts/yuannian1112_jd_scripts/jd_speed_redpocke.js https://raw.githubusercontent.com/veip007/hj/main/jd/faker3/jd_speed_redpocke.js
+#wget -O /root/ql/scripts/yuannian1112_jd_scripts/jd_speed_signfaker.js https://raw.githubusercontent.com/veip007/hj/main/jd/faker3/jd_speed_signfaker.js
+#wget -O /root/ql/scripts/yuannian1112_jd_scripts/jd_speed_signfree.js https://raw.githubusercontent.com/veip007/hj/main/jd/faker3/jd_speed_signfree.js
